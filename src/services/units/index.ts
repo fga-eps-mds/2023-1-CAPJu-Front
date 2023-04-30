016813df717fcf901805b017d92a422ad154f763
senaarth <senaarth@gmail.com>
2023-04-30 19:58:28 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#47) - Cria função de criação de unidade

--- src/services/units/index.ts
+++ src/services/units/index.ts
@@ -2,15 +2,60 @@ import { api } from "services/api";
 
 export const getUnits = async (): Promise<Result<Unit[]>> => {
   try {
-    const res = await api.units.get<Unit[]>("/");
+    const res = await api.units.get<Unit[]>("/units");
 
-    return { type: "success", value: res.data } as unknown as Result<Unit[]>;
+    return { type: "success", value: res.data } as unknown as ResultSuccess<
+      Unit[]
+    >;
   } catch (error) {
-    if (error instanceof Error) return { type: "error", error };
+    if (error instanceof Error)
+      return { type: "error", error, value: undefined };
 
     return {
       type: "error",
       error: new Error("Erro desconhecido"),
+      value: undefined,
+    };
+  }
+};
+
+export const createUnit = async (data: {
+  name: string;
+}): Promise<Result<Unit>> => {
+  try {
+    const res = await api.units.post<Unit>("/newUnit", data);
+
+    return {
+      type: "success",
+      value: res.data,
+    } as unknown as ResultSuccess<Unit>;
+  } catch (error) {
+    if (error instanceof Error)
+      return { type: "error", error, value: undefined };
+
+    return {
+      type: "error",
+      error: new Error("Erro desconhecido"),
+      value: undefined,
+    };
+  }
+};
+
+export const getUnitAdmins = async (
+  unitId: string
+): Promise<Result<User[]>> => {
+  try {
+    const res = await api.units.get<Unit[]>(`/units/${unitId}`);
+
+    return { type: "success", value: res.data } as unknown as Result<User[]>;
+  } catch (error) {
+    if (error instanceof Error)
+      return { type: "error", error, value: undefined };
+
+    return {
+      type: "error",
+      error: new Error("Erro desconhecido"),
+      value: undefined,
     };
   }
 };
