30fd1c77aaff66546c62f3d1b7cba5a324d4a351
senaarth <senaarth@gmail.com>
2023-04-30 23:53:13 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#47) - Adiciona métodos de remoção de unidade e de administrador

--- src/services/units/index.ts
+++ src/services/units/index.ts
@@ -41,13 +41,55 @@ export const createUnit = async (data: {
   }
 };
 
+export const deleteUnit = async (idUnit: string): Promise<Result<any>> => {
+  try {
+    const res = await api.units.delete<Unit>("/deleteUnit", {
+      data: { idUnit },
+    });
+
+    return {
+      type: "success",
+      value: res.data,
+    } as unknown as ResultSuccess<Unit>;
+  } catch (error) {
+    if (error instanceof Error)
+      return { type: "error", error, value: undefined };
+
+    return {
+      type: "error",
+      error: new Error("Erro desconhecido"),
+      value: undefined,
+    };
+  }
+};
+
 export const getUnitAdmins = async (
-  unitId: string
+  unitId: number
 ): Promise<Result<User[]>> => {
   try {
-    const res = await api.units.get<Unit[]>(`/units/${unitId}`);
+    const res = await api.units.get<Unit[]>(`/unitAdmins/${unitId}`);
 
     return { type: "success", value: res.data } as unknown as Result<User[]>;
+  } catch (error) {
+    if (error instanceof Error)
+      return { type: "error", error, value: undefined };
+
+    return {
+      type: "error",
+      error: new Error("Erro desconhecido"),
+      value: undefined,
+    };
+  }
+};
+
+export const removeUnitAdmin = async (data: {
+  idUnit: number;
+  cpf: string;
+}): Promise<Result<null>> => {
+  try {
+    await api.units.put("/removeUnitAdmin/", { ...data });
+
+    return { type: "success", value: null };
   } catch (error) {
     if (error instanceof Error)
       return { type: "error", error, value: undefined };
