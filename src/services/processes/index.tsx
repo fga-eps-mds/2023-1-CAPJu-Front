d4ab86db0ddf9ba5cea87ede8d216325091cc92e
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-05-16 15:01:49 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#112) - Adiciona endpoint de deleção de processo

--- src/services/processes/index.tsx
+++ src/services/processes/index.tsx
@@ -17,3 +17,24 @@ export const getProcesses = async (): Promise<Result<Process[]>> => {
     };
   }
 };
+
+export const deleteProcess = async (
+  registro: string
+): Promise<Result<Process>> => {
+  try {
+    const res = await api.processes.delete<Stage>(`/deleteProcess/${registro}`);
+    return {
+      type: "success",
+      value: res.data,
+    } as unknown as ResultSuccess<Process>;
+  } catch (error) {
+    if (error instanceof Error)
+      return { type: "error", error, value: undefined };
+
+    return {
+      type: "error",
+      error: new Error("Erro desconhecido"),
+      value: undefined,
+    };
+  }
+};
