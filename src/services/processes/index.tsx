c63fb324cd9b055d23de362c5f16b5ee119ed029
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-05-18 01:41:40 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#112) - Adiciona serviço de edição de processo

--- src/services/processes/index.tsx
+++ src/services/processes/index.tsx
@@ -63,3 +63,28 @@ export const createProcess = async (data: {
     };
   }
 };
+
+export const updateProcess = async (data: {
+  record: string;
+  nickname: string;
+  idFlow: number;
+  priority: number;
+  effectiveDate: Date;
+}): Promise<Result<Stage>> => {
+  try {
+    const res = await api.stages.put<Process>("/updateProcess", data);
+    return {
+      type: "success",
+      value: res.data,
+    } as unknown as ResultSuccess<Stage>;
+  } catch (error) {
+    if (error instanceof Error)
+      return { type: "error", error, value: undefined };
+
+    return {
+      type: "error",
+      error: new Error("Erro desconhecido"),
+      value: undefined,
+    };
+  }
+};
