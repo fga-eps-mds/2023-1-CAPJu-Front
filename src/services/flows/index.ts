84a11cc179a45d7fe1e7c0a199a4bd00bfeed108
senaarth <senaarth@gmail.com>
2023-05-19 03:25:35 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#133) - Cria service de fluxos com rotas de criação, remoção e listagem

---
+++ src/services/flows/index.ts
@@ -0,0 +1,57 @@
+import { api } from "services/api";
+
+export const getFlows = async (): Promise<Result<Flow[]>> => {
+  try {
+    const res = await api.flows.get<Flow[]>("/flows");
+
+    return { type: "success", value: res.data };
+  } catch (error) {
+    if (error instanceof Error)
+      return { type: "error", error, value: undefined };
+
+    return {
+      type: "error",
+      error: new Error("Erro desconhecido"),
+      value: undefined,
+    };
+  }
+};
+
+export const createFlow = async (data: {
+  name: string;
+  sequences: FlowSequence[];
+  idUsersToNotify: (string | number)[];
+  idUnit: number;
+}) => {
+  try {
+    const res = await api.flows.post<Flow>(`/newFlow/`, data);
+
+    return { type: "success", value: res.data };
+  } catch (error) {
+    if (error instanceof Error)
+      return { type: "error", error, value: undefined };
+
+    return {
+      type: "error",
+      error: new Error("Erro desconhecido"),
+      value: undefined,
+    };
+  }
+};
+
+export const deleteFlow = async (id: number) => {
+  try {
+    const res = await api.flows.delete<Flow[]>(`/flow/${id}`);
+
+    return { type: "success", value: res.data };
+  } catch (error) {
+    if (error instanceof Error)
+      return { type: "error", error, value: undefined };
+
+    return {
+      type: "error",
+      error: new Error("Erro desconhecido"),
+      value: undefined,
+    };
+  }
+};
