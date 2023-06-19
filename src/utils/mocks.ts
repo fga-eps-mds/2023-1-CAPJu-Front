d93d85543c7ab3153c75318b78789d180722d69f
senaarth <senaarth@gmail.com>
2023-06-18 22:38:14 -0300

    Cria testes de integração da página ViewProcess

--- src/utils/mocks.ts
+++ src/utils/mocks.ts
@@ -35,3 +35,78 @@ export const mockedUnits = [
     name: "Unidade 5",
   },
 ];
+
+export const mockedStages = [
+  {
+    idStage: 1,
+    name: "a",
+    duration: 5,
+    idUnit: 1,
+  },
+  {
+    idStage: 2,
+    name: "b",
+    duration: 5,
+    idUnit: 1,
+  },
+  {
+    idStage: 3,
+    name: "c",
+    duration: 5,
+    idUnit: 1,
+  },
+];
+
+export const mockedFlow = {
+  idFlow: 1,
+  name: "Fluxo 1",
+  idUnit: 1,
+  stages: [1, 2],
+  sequences: [{ from: 1, commentary: "Comentário", to: 2 }],
+};
+
+export const mockedNotStartedProcess = {
+  effectiveDate: null,
+  idFlow: 1,
+  idPriority: 0,
+  idStage: null,
+  idUnit: 1,
+  nickname: "Apelido do Processo",
+  record: "12345678912345678915",
+  status: "notStarted",
+};
+
+export const mockedPriorities = [
+  {
+    idPriority: 1,
+    description: "Art. 1048, II. Do CPC (ECA)",
+  },
+  {
+    idPriority: 2,
+    description: "Art. 1048, IV do CPC (Licitação)",
+  },
+  {
+    idPriority: 3,
+    description: "Art. 7, parágrafo 4, da Lei n 12.016/2009",
+  },
+  {
+    idPriority: 4,
+    description: "Idosa(a) maior de 80 anos",
+  },
+  {
+    idPriority: 5,
+    description: "Pessoa com deficiencia",
+  },
+  {
+    idPriority: 6,
+    description: "Pessoa em situação de rua",
+  },
+  {
+    idPriority: 7,
+    description: "Portador(a) de doença grave",
+  },
+  {
+    idPriority: 8,
+    description: "Réu Preso",
+  },
+];
