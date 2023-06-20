24f801c3e9f60f2641e836b9d5caecf957551b26
GuilhermeDSa1013 <guilhermedsa.spike13@gmail.com>
2023-06-20 15:15:32 -0300

    Adiciona teste de abertura e fechamento da modal de criação do processo

--- src/pages/__tests__/Processes.test.tsx
+++ src/pages/__tests__/Processes.test.tsx
@@ -1,6 +1,12 @@
 import { describe, expect } from "vitest";
-import { act, render, screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
+import {
+  render,
+  screen,
+  fireEvent,
+  waitFor,
+  act,
+} from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
 import { QueryClient, QueryClientProvider } from "react-query";
@@ -51,9 +57,9 @@ describe("Processes page", () => {
           <LoadingProvider>
             <QueryClientProvider client={queryClient}>
               <AuthProvider>
-                <MemoryRouter>
+                <BrowserRouter>
                   <Processes />
-                </MemoryRouter>
+                </BrowserRouter>
               </AuthProvider>
             </QueryClientProvider>
           </LoadingProvider>
@@ -68,5 +74,88 @@ describe("Processes page", () => {
 
   it("renders correctly", () => {
     expect(screen).toMatchSnapshot();
+  });
+
+  it("shows text content correctly", async () => {
+    expect(await screen.findAllByText("Processos")).not.toBe(null);
+    expect(await screen.findByText("Registro")).not.toBe(null);
+    expect(await screen.findByText("Apelido")).not.toBe(null);
+    expect(await screen.findByText("Situação atual")).not.toBe(null);
+    expect(await screen.findByText("Fluxo")).not.toBe(null);
+    expect(await screen.findByText("Status")).not.toBe(null);
+    expect(await screen.findByText("Ações")).not.toBe(null);
+  });
+
+  it("shows process text content correctly", async () => {
+    expect(await screen.findByText("12345678912345678915")).not.toBe(null);
+    expect(await screen.findByText("Processo não Iniciado")).not.toBe(null);
+    expect(await screen.findByText("Não iniciado")).not.toBe(null);
+    expect(await screen.findByText("12345678912345678916")).not.toBe(null);
+    expect(await screen.findByText("Processo Arquivado")).not.toBe(null);
+    expect(await screen.findByText("Arquivado")).not.toBe(null);
+    expect(await screen.findByText("12345678912345678917")).not.toBe(null);
+    expect(await screen.findByText("Processo em Andamento")).not.toBe(null);
+    expect(await screen.findByText("Em andamento")).not.toBe(null);
+    expect(await screen.findByText("12345678912345678918")).not.toBe(null);
+    expect(await screen.findByText("Processo Finalizado")).not.toBe(null);
+    expect(await screen.findByText("Finalizado")).not.toBe(null);
+  });
+
+  it("shows 'create process' correctly", async () => {
+    const button = screen.getByText("Criar Processo");
+
+    if (button) {
+      expect(button).not.toBe(null);
+    }
+  });
+
+  it("shows 'legal priority' correctly", async () => {
+    const button = screen.getByText(
+      "Mostrar apenas processos com prioridade legal"
+    );
+
+    if (button) {
+      expect(button).not.toBe(null);
+    }
+  });
+
+  it("shows 'archived/finished' correctly", async () => {
+    const button = screen.getByText(
+      "Mostrar apenas processos arquivados/finalizados"
+    );
+
+    if (button) {
+      expect(button).not.toBe(null);
+    }
+  });
+
+  it("shows 'search bar' correctly", async () => {
+    const button = screen.getByPlaceholderText(
+      "Pesquisar processos (por registro ou apelido)"
+    );
+
+    if (button) {
+      expect(button).not.toBe(null);
+    }
+  });
+
+  it("opens and closes the creation modal correctly", async () => {
+    const createProcessButton = await screen.getByText("Criar Processo");
+
+    await act(async () => {
+      await fireEvent.click(createProcessButton);
+    });
+
+    expect(await screen.getByPlaceholderText("N do Registro")).not.toBeNull();
+
+    const closeModalButton = await screen.getByText("Cancelar");
+
+    await act(async () => {
+      await fireEvent.click(closeModalButton);
+    });
+
+    await waitFor(() => {
+      expect(screen.queryByText("CreationModal")).toBeNull();
+    });
   });
 });
