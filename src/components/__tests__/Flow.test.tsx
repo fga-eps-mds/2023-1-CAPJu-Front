8bae74d96e85a6c8f89ab20f27341508aeaa867c
harry-cmartin <harrysoncampos@gmail.com>
2023-06-22 22:01:13 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#68) - Adiciona teste Flow a renderização dos componentes.
    Co-authored-by: GuilhermeDSa1013 <guilhermedsa.spike13@gmail.com>

--- src/components/__tests__/Flow.test.tsx
+++ src/components/__tests__/Flow.test.tsx
@@ -1,6 +1,6 @@
 import ResizeObserver from "resize-observer-polyfill";
 import { act, render, screen } from "@testing-library/react";
-import { mockedStage, mockedFlowSequence } from "utils/mocks";
+import { mockedStages, mockedFlowSequence } from "utils/mocks";
 import { Flow } from "../Flow";
 
 describe("Flow components", async () => {
@@ -11,7 +11,7 @@ describe("Flow components", async () => {
   beforeEach(async () => {
     await act(async () => {
       await render(
-        <Flow stages={mockedStage} sequences={mockedFlowSequence} />
+        <Flow stages={mockedStages} sequences={mockedFlowSequence} />
       );
     });
   });
@@ -21,8 +21,37 @@ describe("Flow components", async () => {
   });
 
   it("shows text content correctly", async () => {
-    expect(screen.findAllByAltText(""));
+    expect(
+      screen.findAllByAltText(
+        "Vencimento: 5 Dias úteis após a data de entrada nesta etapa"
+      )
+    ).not.toBe(null);
+    expect(
+      screen.findAllByAltText(
+        "Vencimento: 3 Dias úteis após a data de entrada nesta etapa"
+      )
+    ).not.toBe(null);
+    expect(
+      screen.findAllByAltText(
+        "Vencimento: 1 Dia útil após a data de entrada nesta etapa"
+      )
+    ).not.toBe(null);
   });
 
-  it("", async () => {});
+  it("shows 'stage's properties' correctly", async () => {
+    await mockedStages.forEach(async (stage) => {
+      expect(await screen.findByText(stage.name)).toBeDefined();
+    });
+    await mockedStages.forEach(async (stage) => {
+      expect(await screen.findAllByText(stage.duration)).toBeDefined();
+    });
+  });
+
+  // it("shows 'flow sequence' correctly", async () => {
+  //   const from = await mockedFlowSequence.forEach(async (stage) => {
+  //     expect(await screen.getByText(stage.from)).toBeDefined()});
+  //   const to = await mockedFlowSequence.forEach(async (stage) => {
+  //     expect(await screen.getByText(stage.to)).toBeDefined()});
+
+  //   });
 });
