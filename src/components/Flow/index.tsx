e9d49e6dbcb16b24c8eae6189aed8b28ef40eed6
senaarth <senaarth@gmail.com>
2023-06-25 01:05:25 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#185) - Adiciona novo tipo de aresta para receber arestas com comentários
    Co-authored-by: AnaKarolinyCavalcanti <karol.cavalcanti37@gmail.com>
    Co-authored-by: Juan-Ricarte <juanricarte77@gmail.com>

--- src/components/Flow/index.tsx
+++ src/components/Flow/index.tsx
@@ -11,6 +11,7 @@ import "reactflow/dist/style.css";
 import _ from "lodash";
 
 import { colors } from "styles/colors";
+import { CommentEdge } from "./CommentEdge";
 
 interface FlowProps {
   stages: Stage[];
@@ -22,6 +23,9 @@ interface FlowProps {
   effectiveDate?: string | undefined;
   isFetching?: boolean;
   showStagesDuration?: boolean;
+  processRecord?: string;
+  refetch?: () => void;
+  allowComments?: boolean;
 }
 
 export function Flow({
@@ -34,34 +38,11 @@ export function Flow({
   effectiveDate = undefined,
   isFetching = false,
   showStagesDuration = false,
+  processRecord = undefined,
+  refetch = () => {},
+  allowComments = false,
 }: FlowProps) {
   const startDate = effectiveDate ? new Date(effectiveDate) : null;
-  const sortedStages = useMemo(() => {
-    const sequenceMap = new Map<number, number>();
-
-    sequences.forEach((sequence, index) => {
-      sequenceMap.set(sequence.from, index);
-    });
-
-    return stages.sort((a, b) => {
-      const indexA = sequenceMap.get(a.idStage);
-      const indexB = sequenceMap.get(b.idStage);
-
-      if (indexA === undefined && indexB === undefined) {
-        return 0;
-      }
-
-      if (indexA === undefined) {
-        return 1;
-      }
-
-      if (indexB === undefined) {
-        return -1;
-      }
-
-      return indexA - indexB;
-    });
-  }, [stages, sequences]);
 
   const getStageDeadline = (item: Stage, index: number): string | null => {
     if (!startDate || !effectiveDate) return null;
@@ -85,59 +66,88 @@ export function Flow({
     });
   };
 
-  const nodes = sortedStages.map((item, index) => {
-    const deadline = getStageDeadline(item, index);
-    const stageLabel = `${_.startCase(item.name)}, ${
-      showStagesDuration
-        ? ` (${item.duration} dia${item.duration > 1 ? "s" : ""})`
-        : ""
-    }`;
-    return {
-      id: `${item.idStage}`,
-      position: {
-        x: 30 + index * 60,
-        y: 30 + index * (currentStage ? 125 : 100),
-      },
-      data: {
-        label: (
-          <>
-            {stageLabel}
-            {deadline ? (
-              <>
-                {" "}
-                <br /> {`Vencimento: ${deadline}`}{" "}
-              </>
-            ) : null}
-          </>
-        ),
-      },
-      style: {
-        ...(currentStage === item.idStage
-          ? {
-              color: "white",
-              background: colors.green["500"],
-              fontWeight: "bold",
-            }
-          : {
-              background: colors.gray["200"],
-            }),
-        width: deadline ? 240 : 180,
-      },
-    } as Node;
-  });
+  const nodes = useMemo(() => {
+    return stages.map((item, index) => {
+      const deadline = getStageDeadline(item, index);
+      const stageLabel = `${_.startCase(item.name)}, ${
+        showStagesDuration
+          ? ` (${item.duration} dia${item.duration > 1 ? "s" : ""})`
+          : ""
+      }`;
+      return {
+        id: `${item.idStage}`,
+        position: {
+          x: 30 + index * 60,
+          y: 30 + index * (currentStage ? 225 : 150),
+        },
+        data: {
+          label: (
+            <>
+              {stageLabel}
+              {deadline ? (
+                <>
+                  {" "}
+                  <br /> {`Vencimento: ${deadline}`}{" "}
+                </>
+              ) : null}
+            </>
+          ),
+        },
+        style: {
+          ...(currentStage === item.idStage
+            ? {
+                color: "white",
+                background: colors.green["500"],
+                fontWeight: "bold",
+              }
+            : {
+                background: colors.gray["200"],
+              }),
+          width: deadline ? 280 : 200,
+          minHeight: currentStage ? 80 : 60,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          fontSize: 13,
+        },
+      } as Node;
+    });
+  }, [stages, sequences, currentStage]);
 
-  const edges = sequences.map((item) => {
-    return {
-      id: `edge-${item.from}-${item.to}`,
-      source: `${item.from}`,
-      target: `${item.to}`,
-      animated: currentStage === item.from,
-      style: {
-        stroke: currentStage === item.from ? "#1b9454" : colors.gray["200"],
-      },
-      markerEnd: { type: MarkerType.ArrowClosed, color: "black" },
-    } as Edge;
-  });
+  const edges = useMemo(() => {
+    return sequences.map((sequence: FlowSequence) => {
+      const to = stages.find((item) => item.idStage === sequence.to);
+      const current = stages.find((item) => item.idStage === currentStage);
+
+      return {
+        id: `edge-${sequence.from}-${sequence.to}`,
+        source: `${sequence.from}`,
+        target: `${sequence.to}`,
+        animated: currentStage === sequence.from,
+        style: {
+          stroke:
+            currentStage === sequence.from ? "#1b9454" : colors.gray["200"],
+        },
+        markerEnd: { type: MarkerType.ArrowClosed, color: "black" },
+        type:
+          allowComments &&
+          to &&
+          current &&
+          stages.indexOf(to) <= stages.indexOf(current) + 1
+            ? "commentable"
+            : null,
+        data: {
+          ...sequence,
+          processRecord,
+          refetch,
+        },
+      } as Edge;
+    });
+  }, [stages, sequences, currentStage]);
+
+  const edgeTypes = {
+    commentable: CommentEdge,
+  };
 
   return isFetching ? (
     <Skeleton w={width} maxW={maxWidth} h={minHeight} />
@@ -151,7 +161,7 @@ export function Flow({
           minH={minHeight}
           css={{ "> *": { flex: 1 } }}
         >
-          <ReactFlow nodes={nodes} edges={edges} fitView>
+          <ReactFlow nodes={nodes} edges={edges} edgeTypes={edgeTypes} fitView>
             <Controls />
             <Background />
           </ReactFlow>
