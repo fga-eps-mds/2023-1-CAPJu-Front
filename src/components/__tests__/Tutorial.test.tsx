29c7622e6e3ce4b2513fafe20a369f5320d74f83
GustavoFBS <guguboasorte@gmail.com>
2023-07-02 20:00:05 -0300

    Adiciona testes de texto e corrige alguns
    Co-authored-by: Flávio Melo <flavioovatsug@gmail.com>

--- src/components/__tests__/Tutorial.test.tsx
+++ src/components/__tests__/Tutorial.test.tsx
@@ -2,45 +2,93 @@ import { act, fireEvent, render, screen } from "@testing-library/react";
 import { Tutorial } from "components/Tutorial";
 
 describe("Flow components", async () => {
-    beforeEach(async () => {
-        await act(async () => {
-            await render(
-                <Tutorial />
-            );
-        });
+  beforeEach(async () => {
+    await act(async () => {
+      await render(<Tutorial />);
+    });
+  });
+
+  it("renders correctly", async () => {
+    expect(screen).toMatchSnapshot();
+  });
+
+  it("shows tutorial button correctly", async () => {
+    const tutorialButton = screen.getByTitle("Tutorial");
+
+    await act(async () => {
+      await fireEvent.click(tutorialButton);
     });
 
-    it("renders correctly", async () => {
-        expect(screen).toMatchSnapshot();
-    })
+    const unitContent = screen.getByText("Unidades");
 
-    it("shows tutorial button correctly", async () => {
-        const button = screen.getByTitle("Tutorial");
-     
-        expect(button).not.toBe(null);
-
-        await act(async () => {
-            await fireEvent.click(button);
-        });
-
-        const conteudo = screen.getByText("Unidades");
-
-        expect(conteudo).not.toBe(null);
-
-        await act(async () => {
-            await fireEvent.click(conteudo);
-        });
-
-        expect(await screen.getByText("Unidades representam setores da Justiça Federal, como, por exemplo, uma Vara Judicial.")).not.toBe(null);
-
-        const sair = screen.getByText("Fechar");
-
-        expect(sair).not.toBe(null);
-
-        await act(async () => {
-            await fireEvent.click(sair);
-        });
-        
+    await act(async () => {
+      await fireEvent.click(unitContent);
     });
 
+    expect(
+      await screen.getByText(
+        "Unidades representam setores da Justiça Federal, como, por exemplo, uma Vara Judicial."
+      )
+    ).not.toBe(null);
+
+    const stageContent = screen.getByText("Etapas");
+
+    await act(async () => {
+      await fireEvent.click(stageContent);
+    });
+
+    expect(
+      await screen.getByText(
+        "Etapas representam as fases de um Processo em um determinado Fluxo de trabalho."
+      )
+    ).not.toBe(null);
+
+    const flowContent = screen.getByText("Fluxos");
+
+    await act(async () => {
+      await fireEvent.click(flowContent);
+    });
+
+    expect(
+      await screen.getByText(
+        "Fluxos representam a sequência de Etapas seguida por um Processo."
+      )
+    ).not.toBe(null);
+
+    const processContent = screen.getByText("Processos");
+
+    await act(async () => {
+      await fireEvent.click(processContent);
+    });
+
+    expect(
+      await screen.getByText(
+        "Processos podem ser judiciais (tramitam no sistema PJe) ou administrativos (tramitam no sistema SEI)."
+      )
+    ).not.toBe(null);
+
+    const signupContent = screen.getByText("Cadastro");
+
+    await act(async () => {
+      await fireEvent.click(signupContent);
+    });
+
+    expect(
+      await screen.getByText(
+        "Na página de Cadastro é possível visualizar as solicitações de acesso ao sistema e os acessos concedidos."
+      )
+    ).not.toBe(null);
+
+    const editAccountContent = screen.getByText("Editar Conta");
+
+    await act(async () => {
+      await fireEvent.click(editAccountContent);
+    });
+
+    const closeTutorialButton = screen.getByText("Fechar");
+
+    await act(async () => {
+      await fireEvent.click(closeTutorialButton);
+    });
+  });
 });
