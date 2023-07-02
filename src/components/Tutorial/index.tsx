64a864849d22bfb2c0a84599b24dd92142fdd063
harry-cmartin <harrysoncampos@gmail.com>
2023-07-02 17:49:53 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#83) - Adiciona componente de texto.
    Co-authored-by: Gustavo França <guguboasorte@gmail.com>
    Co-authored-by: Flávio Melo <flavioovatsug@gmail.com>
    Co-authored-by: victordsantoss <victor.samuelsantoss@gmail.com>
    Co-authored-by: GuilhermeDSa1013 <guilhermedsa.spike13@gmail.com>

--- src/components/Tutorial/index.tsx
+++ src/components/Tutorial/index.tsx
@@ -14,6 +14,7 @@ import {
 import { MdHelp } from "react-icons/md";
 import { colors } from "styles/colors";
 import { helperText } from "../../utils/helperText";
+import { Item } from "./Accordion";
 
 export function Tutorial() {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -40,7 +41,15 @@ export function Tutorial() {
           <DrawerBody>
             {helperText &&
               helperText.map((text) => {
-                return <Box key={text.title}>{text.title}</Box>;
+                return (
+                  <Box key={text.title}>
+                    <Item
+                      title={text.title}
+                      subtitle={text.subtitle}
+                      content={text.content && text.content}
+                    />
+                  </Box>
+                );
               })}
           </DrawerBody>
 
