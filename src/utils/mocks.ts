e1d3973034173627f3734409fa352a25632c2929
Oscar de Brito <oscarcorreiadebrito@gmail.com>
2023-07-08 16:44:17 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#95) - Adiciona mock para o teste da DataUpdateModal
    Co-authored-by: leomitx10 <leandroalmeidaoliveira650@gmail.com>
    Co-authored-by: zpeniel09 <zpeniel09@gmail.com>
    Co-authored-by: Vinícius Vieira de Souza <vd.souza1999@gmail.com>
    Co-authored-by: victordsantoss <victor.samuelsantoss@gmail.com>

--- src/utils/mocks.ts
+++ src/utils/mocks.ts
@@ -32,6 +32,16 @@ export const mockedUser = {
   expiresIn: "2100-06-19T03:26:25.050Z",
 };
 
+export const mockedUserDateUpdate = {
+  cpf: "12345678900",
+  fullName: "User Teste",
+  email: "test@test.com",
+  idUnit: 1,
+  token: "mocked-token",
+  idRole: 1,
+  expiresIn: new Date("2100-06-19T03:26:25.050Z"),
+};
+
 export const mockedAdminUser = {
   ...mockedUser,
   idRole: 5,
