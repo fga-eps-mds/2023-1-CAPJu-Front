0941239061eb856195d706363a5ef8e82ab3fe04
GustavoFBS <guguboasorte@gmail.com>
2023-07-13 23:51:14 -0300

    Adiciona teste de editar etapa

--- src/pages/__tests__/Stages.test.tsx
+++ src/pages/__tests__/Stages.test.tsx
@@ -1,5 +1,11 @@
 import { describe, expect } from "vitest";
-import { act, render, screen, fireEvent } from "@testing-library/react";
+import {
+  act,
+  render,
+  screen,
+  fireEvent,
+  waitFor,
+} from "@testing-library/react";
 
 import { BrowserRouter } from "react-router-dom";
 import { rest } from "msw";
@@ -107,31 +113,51 @@ describe("Stages page", () => {
     expect(await screen.queryByText("j")).toBe(null);
   });
 
-  it("filters stages correctly", async () => {
-    const input = screen.getByPlaceholderText("Pesquisar etapas");
-
-    expect(input).not.toBe(null);
+  it("open and closes the edition modal correctly", async () => {
+    const editStageButton = screen.getAllByLabelText("Editar Etapa");
 
     await act(async () => {
-      await fireEvent.change(input, {
+      await fireEvent.click(editStageButton[0]);
+    });
+
+    expect(await screen.findAllByText("Editar etapa")).not.toBeNull();
+
+    const closeModalButton = await screen.getByText("Cancelar");
+
+    await act(async () => {
+      await fireEvent.click(closeModalButton);
+    });
+
+    await waitFor(() => {
+      expect(screen.queryByText("Editar etapa")).toBeNull();
+    });
+  });
+
+  it("filters stages correctly", async () => {
+    const searchStagesButton = screen.getByPlaceholderText("Pesquisar etapas");
+
+    expect(searchStagesButton).not.toBe(null);
+
+    await act(async () => {
+      await fireEvent.change(searchStagesButton, {
         target: { value: "a" },
       });
-      await fireEvent.submit(input);
+      await fireEvent.submit(searchStagesButton);
     });
 
     expect(await screen.queryByText("a")).not.toBe(null);
     expect(await screen.queryByText("b")).toBe(null);
     expect(await screen.queryByText("c")).toBe(null);
 
-    const button = screen.getByLabelText("botão de busca");
+    const searchBarButton = screen.getByLabelText("botão de busca");
 
-    expect(button).not.toBe(null);
+    expect(searchBarButton).not.toBe(null);
 
     await act(async () => {
-      await fireEvent.change(input, {
+      await fireEvent.change(searchStagesButton, {
         target: { value: "c" },
       });
-      await fireEvent.click(button);
+      await fireEvent.click(searchBarButton);
     });
 
     expect(await screen.queryByText("a")).toBe(null);
