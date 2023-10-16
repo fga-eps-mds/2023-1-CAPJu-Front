da62db0d0a57f900933d5f04546255b6cc15f61a
João Gabriel de Matos <joaogabriel.c.matos@gmail.com>
2023-10-16 15:51:36 -0300

    #22 Adds statistics page to navigation.

---
+++ src/pages/Statistics/index.tsx
@@ -0,0 +1,9 @@
+import { PrivateLayout } from "layouts/Private";
+
+export default function Statistics() {
+  return (
+    <PrivateLayout>
+      <div>Estatisticas</div>
+    </PrivateLayout>
+  );
+}
