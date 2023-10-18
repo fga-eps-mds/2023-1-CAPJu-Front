432f04bfa952665ea3d42d1832abbea8ac9178af
Amanda De Moura <amandstudy@gmail.com>
2023-10-18 20:21:00 -0300

    Criando a estrutura do accordion
    Co-authored-by: Denniel William <dennielwilliam.unb@gmail.com>

--- src/pages/Statistics/index.tsx
+++ src/pages/Statistics/index.tsx
@@ -1,5 +1,58 @@
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Flex,
+  Text,
+} from "@chakra-ui/react";
 import { PrivateLayout } from "layouts/Private";
 
 export default function Statistics() {
-  return <PrivateLayout>index</PrivateLayout>;
+  return (
+    <PrivateLayout>
+      <Flex w="90%" maxW={1120} flexDir="column" gap="3" mb="4">
+        <Flex w="100%" justifyContent="space-between" gap="2" flexWrap="wrap">
+          <Text
+            fontSize="22px"
+            fontWeight="600"
+            fontStyle="normal"
+            fontFamily="Inter"
+            lineHeight="24px"
+          >
+            Estatísticas
+          </Text>
+        </Flex>
+        <Box backgroundColor="#FFF" borderRadius="8px">
+          <Flex justifyContent="flex-start" w="100%">
+            <Accordion defaultIndex={[0]} allowMultiple>
+              <AccordionItem>
+                <h2>
+                  <AccordionButton>
+                    <AccordionIcon />
+                    <Box
+                      as="span"
+                      flex="1"
+                      textAlign="left"
+                      marginLeft="18"
+                      fontSize="17px"
+                      fontWeight="600"
+                      fontStyle="normal"
+                      fontFamily="Inter"
+                      lineHeight="24px"
+                    >
+                      Visualizar quantidade de processos em cada etapa
+                    </Box>
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}></AccordionPanel>
+              </AccordionItem>
+            </Accordion>
+          </Flex>
+        </Box>
+      </Flex>
+    </PrivateLayout>
+  );
 }
