ec2ef209bd6d048739b41ffddbf09b5d15ea1f23
Amanda De Moura <amandstudy@gmail.com>
2023-10-20 00:14:36 -0300

    Adicionando os fluxos como opções e botao vai para selecionar etapa
    Co-authored-by: Bruno Ricardo <brunomenezes1511@gmail.com>
    Co-authored-by: Denniel William <dennielwilliam.unb@gmail.com>

--- src/pages/Statistics/index.tsx
+++ src/pages/Statistics/index.tsx
@@ -8,10 +8,40 @@ import {
   Flex,
   Text,
   Button,
+  useToast,
+  Select,
 } from "@chakra-ui/react";
 import { PrivateLayout } from "layouts/Private";
+import { Fragment, useState } from "react";
+import { useQuery } from "react-query";
+import { getFlows } from "services/processManagement/flows";
 
 export default function Statistics() {
+  const toast = useToast();
+
+  const { data: flowsData, isFetched: isFlowsFetched } = useQuery({
+    queryKey: ["flows"],
+    queryFn: async () => {
+      const res = await getFlows();
+
+      if (res.type === "error") throw new Error(res.error.message);
+
+      return res;
+    },
+    onError: () => {
+      toast({
+        id: "flows-error",
+        title: "Erro ao carregar fluxos",
+        description:
+          "Houve um erro ao carregar fluxos, favor tentar novamente.",
+        status: "error",
+        isClosable: true,
+      });
+    },
+  });
+
+  var [bool, setBool] = useState(isFlowsFetched);
+
   return (
     <PrivateLayout>
       <Flex w="90%" maxW={1120} flexDir="column" gap="3" mb="4">
@@ -27,14 +57,13 @@ export default function Statistics() {
         </Flex>
         <Box backgroundColor="#FFF" borderRadius="8px">
           <Flex justifyContent="flex-start" w="100%">
-            <Accordion defaultIndex={[0]} allowMultiple>
+            <Accordion defaultIndex={[0]} allowMultiple width="100%">
               <AccordionItem>
                 <h2>
                   <AccordionButton>
                     <AccordionIcon />
                     <Box
                       as="span"
-                      flex="1"
                       textAlign="left"
                       marginLeft="18"
                       fontSize="17px"
@@ -46,8 +75,48 @@ export default function Statistics() {
                     </Box>
                   </AccordionButton>
                 </h2>
-                <AccordionPanel pb={4}>
-                  <Button colorScheme="green">Confirmar</Button>
+                <AccordionPanel pb={4} width="100%">
+                  {bool ? (
+                    <Fragment>
+                      <Flex>
+                        <Select
+                          placeholder="Selecione a etapa"
+                          marginLeft="36px"
+                          width="302px"
+                        >
+                          <option>dfsfsf</option>
+                        </Select>
+                        <Button
+                          colorScheme="green"
+                          marginLeft="20px"
+                          onClick={() => setBool(false)}
+                        >
+                          Confirmar
+                        </Button>
+                      </Flex>
+                    </Fragment>
+                  ) : (
+                    <Flex>
+                      <Select
+                        placeholder="Selecione o fluxo"
+                        marginLeft="36px"
+                        width="302px"
+                      >
+                        {flowsData?.value?.map((flow) => (
+                          <option key={flow.idFlow} value={flow.name} >
+                            {flow.name}
+                          </option>
+                        ))}
+                      </Select>
+                      <Button
+                        colorScheme="green"
+                        marginLeft="20px"
+                        onClick={() => setBool(true)}
+                      >
+                        Confirmar
+                      </Button>
+                    </Flex>
+                  )}
                 </AccordionPanel>
               </AccordionItem>
             </Accordion>
