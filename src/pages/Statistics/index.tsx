5d478d23181bc0027de361193bb155ad593f2a72
Davi Rodrigues da Rocha <davirocha12.80@gmail.com>
2023-10-22 16:30:07 -0300

    Criacão do front-end referente a US14

---
+++ src/pages/Statistics/index.tsx
@@ -0,0 +1,10 @@
+import { PrivateLayout } from "layouts/Private";
+import StepDeadlineReports from "./StepDeadlineReports";
+
+export default function Statistics() {
+  return (
+    <PrivateLayout>
+      <StepDeadlineReports />
+    </PrivateLayout>
+  );
+}
