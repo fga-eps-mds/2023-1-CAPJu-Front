e35383b466ceabe3e4f3ac9a0d643b442c26a342
Denniel <dennielwilliam.unb@gmail.com>
2023-11-21 23:40:23 -0300

    Ajustando build

--- src/utils/assets.ts
+++ src/utils/assets.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-unresolved */
 import justicaFederal from "../images/justica_federal.png";
-import logoUnB from "../images/UnB.png";
+import logoUnB from "../images/unb.png";
 import pdf from "../images/pdf.svg";
 
 const assets = {
