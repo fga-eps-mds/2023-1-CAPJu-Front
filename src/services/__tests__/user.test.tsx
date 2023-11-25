971727af95494b970de024694d6af1ccfb8f54b9
João Gabriel de Matos <joaogabriel.c.matos@gmail.com>
2023-11-24 23:46:47 -0300

    Adiciona teste de getUserById de user service.

--- src/services/__tests__/user.test.tsx
+++ src/services/__tests__/user.test.tsx
@@ -1,16 +1,17 @@
 import MockAdapter from "axios-mock-adapter";
 import { api } from "../api";
-import { signIn, signUp } from "../user";
+import { signIn, signUp, getUserById } from "../user";
 
-const mock = new MockAdapter(api.user);
+const apiMockUser = new MockAdapter(api.user);
+const apiMockRole = new MockAdapter(api.role);
 
 describe("Testes para a função signIn", () => {
   afterEach(() => {
-    mock.reset();
+    apiMockUser.reset();
   });
 
   it("Sucesso signIn", async () => {
-    mock.onPost("/login").reply(200, "mock-token");
+    apiMockUser.onPost("/login").reply(200, "mock-token");
 
     const result = await signIn({ cpf: "usuário", password: "senha" });
 
@@ -18,7 +19,7 @@ describe("Testes para a função signIn", () => {
   });
 
   it("Erro signIn", async () => {
-    mock.onPost("/login").reply(401, "Invalid credentials");
+    apiMockUser.onPost("/login").reply(401, "Invalid credentials");
 
     const result = await signIn({ cpf: "usuário", password: "senha" });
 
@@ -28,24 +29,15 @@ describe("Testes para a função signIn", () => {
       value: undefined,
     });
   });
-
-  // it('Erro desconhecido signIn', async () => {
-  //   mock.onPost('/login').reply(500, 'Erro desconhecido');
-
-  //   const result = await signIn({cpf:'usuário', password:'senha'});
-
-  //   expect(result).toEqual({ type: 'error', error: new Error('Erro desconhecido'), value: undefined,
-  // });
-  // });
 });
 
 describe("Testes para a função signUp", () => {
   afterEach(() => {
-    mock.reset();
+    apiMockUser.reset();
   });
 
   it("Sucesso signUp", async () => {
-    mock.onPost("/newUser").reply(200, "dados");
+    apiMockUser.onPost("/newUser").reply(200, "dados");
 
     const result = await signUp({
       fullName: "Joe Fulano",
@@ -61,7 +53,7 @@ describe("Testes para a função signUp", () => {
   });
 
   it("Erro signUp", async () => {
-    mock.onPost("/newUser").reply(500, "Erro ao criar usuário");
+    apiMockUser.onPost("/newUser").reply(500, "Erro ao criar usuário");
 
     const result = await signUp({
       fullName: "Joe Fulano",
@@ -79,20 +71,77 @@ describe("Testes para a função signUp", () => {
       value: undefined,
     });
   });
+});
 
-  // it('Erro desconheido signUp', async () => {
-  //   mock.onPost('/newUser').reply(500, "Erro desconhecido");
+describe("Testes para a função getUserById", () => {
+  afterEach(() => {
+    apiMockUser.reset();
+    apiMockRole.reset();
+  });
 
-  //   const result = await signUp({
-  //     fullName: "Joe Fulano",
-  //     cpf: "11111111111",
-  //     email: "joe@email.com",
-  //     password: "senha",
-  //     passwordConfirmation: "senha",
-  //     idUnit: "1",
-  //     idRole: "1",
-  //   });
+  it("getUserById: usuario existe e não tem idRole", async () => {
+    const id = "1";
+    apiMockUser.onGet(`/cpf/${id}`).reply(200, { nome: "Fulano" });
 
-  //   expect(result).toEqual({ type: 'error',error:new Error('Erro desconhecido'), value: undefined});
-  // });
+    const result = await getUserById(id);
+
+    expect(result).toEqual({
+      type: "success",
+      value: { nome: "Fulano", allowedActions: [] },
+    });
+  });
+
+  it("getUserById: usuario existe e tem idRole, sem allowedActions", async () => {
+    const id = "1";
+    apiMockUser.onGet(`/cpf/${id}`).reply(200, { nome: "Fulano", idRole: 1 });
+
+    const result = await getUserById(id);
+    expect(result).toEqual({
+      type: "success",
+      value: {
+        nome: "Fulano",
+        idRole: 1,
+        allowedActions: [],
+      },
+    });
+  });
+
+  it("getUserById: usuario existe e tem idRole, com allowedActions", async () => {
+    const id = "1";
+    const userData = {
+      nome: "Fulano",
+      idRole: 1,
+    };
+    const role = {
+      roleId: 1,
+      name: "Administrador",
+      allowedActions: ["Ação 1", "Ação 2"],
+    };
+    apiMockUser.onGet(`/cpf/${id}`).reply(200, { ...userData });
+    apiMockRole.onGet(`/roleAdmins/${userData.idRole}`).reply(200, { ...role });
+
+    const result = await getUserById(id);
+    console.log({ result });
+    expect(result).toEqual({
+      type: "success",
+      value: {
+        nome: "Fulano",
+        idRole: 1,
+        allowedActions: role.allowedActions,
+      },
+    });
+  });
+
+  it("getUserById: usuario não existe", async () => {
+    const id = "1";
+    apiMockUser.onGet(`/cpf/${id}`).reply(404, "Usuario não encontrado");
+
+    const result = await getUserById(id);
+
+    expect(result).toEqual({
+      type: "error",
+      value: undefined,
+      error: new Error("Usuario não encontrado"),
+    });
+  });
 });
