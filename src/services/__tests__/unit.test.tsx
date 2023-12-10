119ac1b55e7ad04eef543e00bad6a18f3f8965fd
Brenda Santos <brendasantosv.bs@gmail.com>
2023-12-10 16:34:16 -0300

    Criando teste para funcao updateUnit

--- src/services/__tests__/unit.test.tsx
+++ src/services/__tests__/unit.test.tsx
@@ -1,5 +1,5 @@
 import MockAdapter from "axios-mock-adapter";
-import { getUnits } from "services/unit";
+import { createUnit, getUnits, updateUnit } from "services/unit";
 import { api } from "../api";
 
 const apiMockUnit = new MockAdapter(api.unit);
@@ -45,6 +45,82 @@ describe("Testes para a função getUnits", () => {
     apiMockUnit.onGet("/").reply(400, {});
 
     const result = await getUnits(params.pagination, params.filter);
+
+    expect(result).toEqual({
+      type: "error",
+      value: undefined,
+      error: new Error("Something went wrong"),
+    });
+  });
+});
+
+describe("Testes para a função createUnit", () => {
+  afterEach(() => {
+    apiMockUnit.reset();
+  });
+
+  it("sucesso post /newUnit", async () => {
+    apiMockUnit.onPost("/newUnit").reply(200, {
+      idUnit: 1,
+      name: "Unidade",
+    });
+
+    const result = await createUnit({ name: "Unidade" });
+
+    expect(result).toEqual({
+      type: "success",
+      value: {
+        idUnit: 1,
+        name: "Unidade",
+      },
+    });
+  });
+
+  it("error post /newUnit", async () => {
+    apiMockUnit.onPost("/newUnit").reply(400, {});
+
+    const result = await createUnit({ name: "Unidade" });
+
+    expect(result).toEqual({
+      type: "error",
+      value: undefined,
+      error: new Error("Something went wrong"),
+    });
+  });
+});
+
+describe("Testes para a função updateUnit", () => {
+  afterEach(() => {
+    apiMockUnit.reset();
+  });
+
+  it("sucesso put /updateUnit", async () => {
+    apiMockUnit.onPut("/updateUnit").reply(200, {
+      name: "Unidade",
+      idUnit: 1,
+    });
+
+    const result = await updateUnit({
+      name: "Unidade",
+      idUnit: 1,
+    });
+
+    expect(result).toEqual({
+      type: "success",
+      value: {
+        name: "Unidade",
+        idUnit: 1,
+      },
+    });
+  });
+
+  it("error put /updateUnit", async () => {
+    apiMockUnit.onPut("/updateUnit").reply(400, {});
+
+    const result = await updateUnit({
+      name: "Unidade",
+      idUnit: 1,
+    });
 
     expect(result).toEqual({
       type: "error",
