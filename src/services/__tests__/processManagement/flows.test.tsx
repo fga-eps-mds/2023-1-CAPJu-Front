4c5ef2ba7da43fb6db788c90db38b029faa6b6f9
Camila Careli <camilacsouzza@gmail.com>
2023-12-10 15:04:04 -0300

    Testes para a funcao getFlowById

--- src/services/__tests__/processManagement/flows.test.tsx
+++ src/services/__tests__/processManagement/flows.test.tsx
@@ -1,5 +1,5 @@
 import MockAdapter from "axios-mock-adapter";
-import { getFlows } from "services/processManagement/flows";
+import { getFlows, getFlowById } from "services/processManagement/flows";
 import { api } from "../../api";
 
 const apiMockProcessManagement = new MockAdapter(api.processManagement);
@@ -49,3 +49,29 @@ describe("Testes para a função getFlows", () => {
     });
   });
 });
+
+describe("Testes para a função getFlowById", () => {
+    afterEach(() => {
+      apiMockProcessManagement.reset();
+    });
+  
+    it("sucesso get /flow/:id", async () => {
+      apiMockProcessManagement.onGet("/flow/1").reply(200, { 
+        idProcess: 1
+      });
+  
+      const result = await getFlowById(1)
+  
+      expect(result).toEqual({ type: "success", value: {
+        idProcess: 1
+      }});
+    });
+  
+    it("error get /flow/:id", async () => {
+      apiMockProcessManagement.onGet("/flow/1").reply(400, { });
+  
+      const result = await getFlowById(1)
+  
+      expect(result).toEqual({ type: "error", value: undefined, error: new Error("Something went wrong") });
+    });
+  });
