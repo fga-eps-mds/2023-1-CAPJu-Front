810f1620350ca624e4c6e8467060888a93f5c4a7
Bruno Ricardo <brunomenezes1511@gmail.com>
2023-12-10 22:07:48 -0300

    Testando getPriorities sucess

---
+++ src/services/__tests__/priority.test.tsx
@@ -0,0 +1,48 @@
+import { getPriorities } from "services/processManagement/priority";
+import MockAdapter from "axios-mock-adapter";
+import { api } from "../api";
+
+const apiMockProcessManagement = new MockAdapter(api.processManagement);
+
+describe("Teste para a função getPriorities", () => {
+  afterEach(() => {
+    apiMockProcessManagement.reset();
+  });
+
+  it("Sucesso getPriorities when id is provided", async () => {
+    const prioritiesData = [
+      { idPriority: 1, name: "High" },
+      { idPriority: 2, name: "Medium" },
+    ];
+
+    const targetId = 2;
+    const expectedPriority = prioritiesData.find(
+      (priority) => priority.idPriority === targetId
+    );
+
+    apiMockProcessManagement.onGet("/priority").reply(200, prioritiesData);
+
+    const result = await getPriorities(targetId);
+
+    expect(result).toEqual({
+      type: "success",
+      value: expectedPriority,
+    });
+  });
+
+  it("Sucesso getPriorities when id is not provided", async () => {
+    const prioritiesData = [
+      { idPriority: 1, name: "High" },
+      { idPriority: 2, name: "Medium" },
+    ];
+
+    apiMockProcessManagement.onGet("/priority").reply(200, prioritiesData);
+
+    const result = await getPriorities();
+
+    expect(result).toEqual({
+      type: "success",
+      value: prioritiesData,
+    });
+  });
+});
