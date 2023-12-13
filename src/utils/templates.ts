4c9319eb786edb2f5f5a07cf018a8c430f37fd19
Brenda Santos <brendasantosv.bs@gmail.com>
2023-12-13 16:37:55 -0300

    Ajustando acesso ao arquivo exemplo da importacao.
    Co-authored-by: WashingtonBispo <juniorbispo515@gmail.com>

---
+++ src/utils/templates.ts
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-unresolved */
+import templatexlsx from "../images/modeloImportacaoCapju.xlsx.png";
+
+const template = {
+  templatexlsx,
+};
+
+export default template;
